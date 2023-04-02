adf1a17a2ef417e77b2362251d5ad557ee111167
Gosiek <gosiek.piernik@gmail.com>
2023-04-03 00:33:46 +0200

    Changed types in class Book Record

--- records/book.record.ts
+++ records/book.record.ts
@@ -7,39 +7,39 @@ import {FieldPacket} from "mysql2";
 type BookResults = [BookRecord[], FieldPacket[]];
 
 export class BookRecord implements OneBook {
-    id: string;
+    id?: string;
     title: string;
-    firstAuthor: string;
-    secondAuthor?: string;
+    mainAuthor: string;
     publishedDate: number;
     ISBNNumber: number;
     type: string;
+    dateOfReading: Date;
     opinion: number;
 
     constructor(obj: OneBook) {
-        if (!obj.title || obj.title.length > 100) {
-            throw new ValidationError('Musisz podać tytuł książki. Tytuł powinien zawierać do 100 znaków');
-        }
-        if (!obj.firstAuthor || obj.firstAuthor.length > 100) {
-            throw new ValidationError("Uzupełnij imię i nazwisko autora. Możesz użyć 100 znaków")
-        }
-        // if (obj.publishedDate !== 4) {
-        //     throw new ValidationError('Wpisz rok wydania')
+        // if (!obj.title || obj.title.length > 100) {
+        //     throw new ValidationError('Musisz podać tytuł książki. Tytuł powinien zawierać do 100 znaków');
         // }
-        if (!obj.type) {
-            throw new ValidationError('Wybierz dominujący typ książki')
-        }
-        if (!obj.opinion) {
-            throw new ValidationError('Musisz dokonać oceny przeczytanej książki')
-        }
+        // if (!obj.firstAuthor || obj.firstAuthor.length > 100) {
+        //     throw new ValidationError("Uzupełnij imię i nazwisko autora. Możesz użyć 100 znaków")
+        // }
+        // // if (obj.publishedDate !== 4) {
+        // //     throw new ValidationError('Wpisz rok wydania')
+        // // }
+        // if (!obj.type) {
+        //     throw new ValidationError('Wybierz dominujący typ książki')
+        // }
+        // if (!obj.opinion) {
+        //     throw new ValidationError('Musisz dokonać oceny przeczytanej książki')
+        // }
 
         this.id = obj.id;
         this.title = obj.title;
-        this.firstAuthor = obj.firstAuthor;
-        this.secondAuthor = obj.secondAuthor;
+        this.mainAuthor = obj.mainAuthor;
         this.publishedDate = obj.publishedDate;
         this.ISBNNumber = obj.ISBNNumber;
         this.type = obj.type;
+        this.dateOfReading = obj.dateOfReading;
         this.opinion = obj.opinion;
     }
 
@@ -48,14 +48,14 @@ export class BookRecord implements OneBook {
             this.id = uuid();
         }
 
-        await pool.execute("INSERT INTO `books` VALUES (:id, :title, :firstAuthor, :secondAuthor, :publishedDate, :ISBNNumber, :type, :opinion)", {
+        await pool.execute("INSERT INTO `books` VALUES(:id, :title, :mainAuthor, :publishedDate, :ISBNNumber, :type, :dateOfReading, :opinion)", {
             id: this.id,
             title: this.title,
-            firstAuthor: this.firstAuthor,
-            secondAuthor: this.secondAuthor,
+            mainAuthor: this.mainAuthor,
             publishedDate: this.publishedDate,
             ISBNNumber: this.ISBNNumber,
             type: this.type,
+            dateOfReading: this.dateOfReading,
             opinion: this.opinion
         })
         return this.id;
@@ -66,6 +66,11 @@ export class BookRecord implements OneBook {
         return results.map(obj => new BookRecord(obj));
     }
 
+    static async listAllWithDate(): Promise<BookRecord[]> {
+        const [dateResult] = await pool.execute("SELECT * FROM `books` WHERE `dateOfReading` BETWEEN '2023-01-01' AND '2023-12-31'") as BookResults;
+        return dateResult.map(obj => new BookRecord(obj));
+    }
+
     static async getOne(id: string) {
         const [results] = await pool.execute("SELECT * FROM `books` WHERE `id` = :id ", {
             id
@@ -74,8 +79,10 @@ export class BookRecord implements OneBook {
     }
 
     async delete() {
-        await pool.execute('DELETE FROM `books` WHERE `id` = :id', {
+        await pool.execute("DELETE FROM `books` WHERE `id` = :id", {
             id: this.id,
         });
     }
+
+
 }
